aa0f3c1802dbe86e68bc1535189b4c27317d097d
Chriszao <chris.f.assis18@gmail.com>
2022-12-03 18:28:32 -0300

    feat: Implemented collisions handling

--- hashTable/hashTable/hashTable.go
+++ hashTable/hashTable/hashTable.go
@@ -47,7 +47,17 @@ func (h *Hash) Len() int {
 }
 
 func (h *Hash) Insert(student studentPkg.Student) {
+	if h.IsFull() {
+		fmt.Println("Hash table is full!")
+		return
+	}
+
 	index := h.hashFunction(student)
+
+	// Handling collisions
+	for h.structure[index].GetRA() > 0 {
+		index = (index + 1) % h.maxPositions
+	}
 
 	h.structure[index] = student
 	h.size++
@@ -56,21 +66,30 @@ func (h *Hash) Insert(student studentPkg.Student) {
 func (h *Hash) Remove(student studentPkg.Student) {
 	index := h.hashFunction(student)
 
-	if h.structure[index].GetRA() != -1 {
-		h.structure[index] = *studentPkg.NewWithParams(-1, " ")
-		h.size--
+	for h.structure[index].GetRA() != -1 {
+		if h.structure[index].GetRA() == student.GetRA() {
+			fmt.Println("Student removed!")
+			h.structure[index] = *studentPkg.NewWithParams(-2, " ")
+			h.size--
+			return
+		}
+		index = (index + 1) % h.maxPositions
 	}
+	fmt.Println("Student not found!")
+
 }
 
 func (h *Hash) Find(student *studentPkg.Student, found *bool) {
 	index := h.hashFunction(*student)
-	aux := h.structure[index]
 
-	if aux.GetRA() != student.GetRA() {
-		*found = false
-	} else {
-		*found = true
-		*student = aux
+	for h.structure[index].GetRA() != -1 {
+		if h.structure[index].GetRA() == student.GetRA() {
+			*found = true
+			*student = h.structure[index]
+			break
+		}
+
+		index = (index + 1) % h.maxPositions
 	}
 }
 
@@ -78,7 +97,7 @@ func (h *Hash) Print() {
 	printValue := "Hash table: \n"
 
 	for index := 0; index < h.maxPositions; index++ {
-		if h.structure[index].GetRA() != -1 {
+		if h.structure[index].GetRA() > 0 {
 			printValue += fmt.Sprintf(
 				"Position: %d, RA: %d, Name: %s\n",
 				index,
